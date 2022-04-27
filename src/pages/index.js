35c5d7ac4e35d045ea864640c70c22a13c3b8203
trannguyenvu3482 <21088201.vu@student.iuh.edu.vn>
2022-04-27 14:14:51 +0700

    finish navbar

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from './HomePage';
+import About from './AboutPage';
+import Cart from './CartPage';
+import Checkout from './CheckoutPage';
+import Products from './ProductsPage';
+import SingleProduct from './SingleProductPage';
+import Error from './ErrorPage';
+import PrivateRoute from './PrivateRoute';
+
+export {
+  Home,
+  About,
+  Cart,
+  Checkout,
+  Products,
+  SingleProduct,
+  Error,
+  PrivateRoute,
+};
